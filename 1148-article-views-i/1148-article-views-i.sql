77c6471ac22a330da1bc27766300f686a53796db
XinyueQiu <92532875+xyqiuchloe@users.noreply.github.com>
2022-11-09 16:30:12 -0500

    Time: 665 ms (35.20%), Space: 0B (100.00%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
-select distinct(author_id) as id from Views
-where author_id = viewer_id
-order by 1 ASC
+select distinct author_id as id
+from Views
+where (author_id = viewer_id)
+order by author_id ASC
